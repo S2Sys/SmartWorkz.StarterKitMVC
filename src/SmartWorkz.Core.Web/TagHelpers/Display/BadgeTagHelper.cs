11ffb4c9c5c3beca1a4e4ce1ba88957b9c54458a
Senthilvel Thangavelu <t.senthilvel@hotmail.com>
2026-04-18 19:49:48 +0530

    feat: implement Task 11 - BadgeTagHelper for Bootstrap badges with color variants

---
+++ src/SmartWorkz.Core.Web/TagHelpers/Display/BadgeTagHelper.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace SmartWorkz.Core.Web.TagHelpers.Display;
+
+[HtmlTargetElement("badge", Attributes = nameof(Type))]
+public class BadgeTagHelper : TagHelper
+{
+    [HtmlAttributeName(nameof(Type))]
+    public string Type { get; set; } = "secondary";
+
+    [HtmlAttributeName(nameof(Text))]
+    public string? Text { get; set; }
+
+    public override void Process(TagHelperContext context, TagHelperOutput output)
+    {
+        var badgeClass = Type switch
+        {
+            "primary" => "bg-primary",
+            "success" => "bg-success",
+            "danger" => "bg-danger",
+            "warning" => "bg-warning",
+            "info" => "bg-info",
+            "light" => "bg-light text-dark",
+            "dark" => "bg-dark",
+            _ => "bg-secondary"
+        };
+
+        var html = $"<span class=\"badge {badgeClass}\">{Text}</span>";
+
+        output.TagName = null;
+        output.Content.SetHtmlContent(html);
+    }
+}
