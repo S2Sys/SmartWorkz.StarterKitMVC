a3a7e9230c3a7d0bda075dd94f5f92a1c3a908d7
Senthilvel Thangavelu <t.senthilvel@hotmail.com>
2026-04-21 07:55:41 +0530

    feat: create SmartWorkz.Core.Mobile project skeleton

---
+++ src/SmartWorkz.Core.Mobile/GlobalUsings.cs
@@ -0,0 +1,8 @@
+global using System.Net.Http;
+global using System.Net.Http.Headers;
+global using System.Text.Json;
+global using System.Reactive;
+global using SmartWorkz.Core.Shared.Results;
+global using SmartWorkz.Core.Shared.Guards;
+global using SmartWorkz.Core.Shared.Logging;
+global using SmartWorkz.Core.Mobile;
